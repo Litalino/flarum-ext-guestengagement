9c1de7a21af86a341b1d9c54fc7914e11c9bb51e
Litalino <99712477+Litalino@users.noreply.github.com>
2023-12-11 17:29:56 +0700

    Up

--- js/src/admin/index.js
+++ js/src/admin/index.js
@@ -12,21 +12,21 @@ import app from 'flarum/app';
 import ExtensionPage from 'flarum/admin/components/ExtensionPage';
 
 app.initializers.add('litalino-guestengagement', () => {
-  app.extensionData.for('justoverclock-guestengagement').registerSetting({
+  app.extensionData.for('litalino-guestengagement').registerSetting({
     setting: 'justoverclock-guestengagement.coordinates',
     name: 'afterXpost',
     type: 'number',
     label: app.translator.trans('flarum-ext-guestengagement.admin.afterxpost'),
     help: app.translator.trans('flarum-ext-guestengagement.admin.afterxpostdesc'),
   });
-  app.extensionData.for('justoverclock-guestengagement').registerSetting({
+  app.extensionData.for('litalino-guestengagement').registerSetting({
     setting: 'justoverclock-guestengagement.timeout',
     name: 'timeout',
     type: 'number',
     label: app.translator.trans('flarum-ext-guestengagement.admin.autohide'),
     help: app.translator.trans('flarum-ext-guestengagement.admin.autohidedesc'),
   });
-  app.extensionData.for('justoverclock-guestengagement').registerSetting({
+  app.extensionData.for('litalino-guestengagement').registerSetting({
     setting: 'justoverclock-guestengagement.textchange',
     name: 'tchange',
     type: 'text',
@@ -34,7 +34,7 @@ app.initializers.add('litalino-guestengagement', () => {
     help: app.translator.trans('flarum-ext-guestengagement.admin.changetextboxdesc'),
     placeholder: 'When you create an account...',
   });
-  app.extensionData.for('justoverclock-guestengagement').registerSetting({
+  app.extensionData.for('litalino-guestengagement').registerSetting({
     setting: 'justoverclock-guestengagement.BoxTitleChange',
     name: 'BoxTitle',
     type: 'text',
@@ -42,22 +42,22 @@ app.initializers.add('litalino-guestengagement', () => {
     help: app.translator.trans('flarum-ext-guestengagement.admin.maintitledesc'),
     placeholder: 'Hello! You seem to be interested in this post!',
   });
-  app.extensionData.for('justoverclock-guestengagement').registerSetting({
+  app.extensionData.for('litalino-guestengagement').registerSetting({
     setting: 'justoverclock-guestengagement.hide.homebox',
     label: app.translator.trans('flarum-ext-guestengagement.admin.enablealbox'),
     type: 'boolean',
   });
-  app.extensionData.for('justoverclock-guestengagement').registerSetting({
+  app.extensionData.for('litalino-guestengagement').registerSetting({
     setting: 'justoverclock-guestengagement.hide.postbox',
     label: app.translator.trans('flarum-ext-guestengagement.admin.enablepostbox'),
     type: 'boolean',
   });
-  app.extensionData.for('justoverclock-guestengagement').registerSetting({
+  app.extensionData.for('litalino-guestengagement').registerSetting({
     setting: 'justoverclock-guestengagement.hide.homebox.buttom',
     label: app.translator.trans('flarum-ext-guestengagement.admin.enable_buttom_homepage'),
     type: 'boolean',
   });
-  app.extensionData.for('justoverclock-guestengagement').registerSetting({
+  app.extensionData.for('litalino-guestengagement').registerSetting({
     setting: 'justoverclock-guestengagement.hide.post.buttom',
     label: app.translator.trans('flarum-ext-guestengagement.admin.enable_buttom_post'),
     type: 'boolean',
